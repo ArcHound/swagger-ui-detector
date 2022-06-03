83d45ae3e320cd8110daf2bd2b8dc10046e83bbe
milo <miloslav.homer@kiwi.com>
2022-06-03 15:48:08 +0200

    chore: regroup and snyk_parser unit tests

---
+++ src/git_searcher.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+
+import git
+from packaging import version
+
+import logging
+
+log = logging.getLogger(__name__)
+
+
+class GitSearcher:
+    """Object to search the repository for a hash, returns version of software that has it
+
+    Parameters:
+    repo (string): Path to the git repository containing swagger-ui
+    """
+
+    def __init__(self, repo, special_cases):
+        """Constructor
+
+        Parameters:
+        repo (string): Path to the git repository containing swagger-ui
+        special_casess (dict:string->string): Dictionary of hashes and versions that are not in the repo
+        """
+        self.g = git.cmd.Git(repo)
+        self.special_cases = special_cases
+
+    def get_version_from_shorthash(self, shorthash):
+        """Searches for the shorthash in the repo attempting to return the version containing the commit
+
+        Parameters:
+        shorthash (string): hash of the commit identified in the swagger
+
+        Returns:
+        version (string): version containing commit specified by shorthash
+        """
+        if shorthash in self.special_cases:
+            return self.special_cases[shorthash]
+        versions = self.g.tag(contains=shorthash).split("\n")
+        if "$GIT_TAG" in versions:
+            versions.remove("$GIT_TAG")
+        if len(versions) > 0:
+            versions.sort(key=version.parse)
+            return versions[0]
+        else:
+            log.info(f'Unable to find version for tag "{shorthash}".')
+            return None
